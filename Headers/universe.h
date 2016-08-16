4ab2028f6ee78e0511a9f4b5ddafda9b72e29ff7
runewalsh <runewalsh@users.noreply.github.com>
2016-08-16 12:20:08 +0300

    Update universe.h

--- Headers/universe.h
+++ Headers/universe.h
@@ -1,16 +1,37 @@
-#ifndef UNIVERSE.H
+#ifndef UNIVERSE_H
 #define UNVERSE_H
+#include <vector>
 
-class Universe
+using std::vector;
+using std::size_t;
+
+class universe
 {
 public:
-    Universe(int, int);
-    bool* operator[](int);
-    void nextGen();
+	// массив размером w*h. хранится по строкам слева направо, т. о. точка с координатами (x, y) лежит в y*w+x.
+	typedef vector<bool> data_array;
+	typedef data_array::reference cell_ref;
+
+	universe(int, int);
+    cell_ref operator()(int x, int y);
+	bool get(int x, int y);
+	void set(int x, int y);
+	void unset(int x, int y);
+	void set(int x, int y, bool value);
+    void next_gen();
+	void fill_at_random();
 
 private:
-    const int _WIDTH, _HEIGHT;
-    bool** _grid;
+    const int w, h;
+	// в одном из массивов — текущее поколение, в другом — следующее
+    data_array data1, data2;
+	bool flipped;
+
+	data_array& /*src_*/data();
+	data_array& dest_data();
+
+	bool rangecheck(int x, int y);
+	size_t cell_offset(int x, int y);
 };
 
 #endif //UNIVERSE_H
